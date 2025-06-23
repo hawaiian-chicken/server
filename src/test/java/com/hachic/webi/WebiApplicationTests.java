44dd1d85e0756fec92f62ae4f0883080497554bb
astrokan <astrokan@naver.com>
2025-06-23 15:14:17 +0900

    first commit

---
+++ src/test/java/com/hachic/webi/WebiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hachic.webi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
