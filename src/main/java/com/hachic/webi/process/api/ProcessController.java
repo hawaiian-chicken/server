51f8418a9fb4b7cb90e03cffbe8097a80cde1290
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-09 23:40:58 +0900

    feat: html process 연결 원본 html을 ai server에 넘기고 수정된 값을 response로 보내는 기능, text 수정 필요

---
+++ src/main/java/com/hachic/webi/process/api/ProcessController.java
@@ -0,0 +1,33 @@
+package com.hachic.webi.process.api;
+
+import com.hachic.webi.process.application.ProcessService;
+import com.hachic.webi.process.dto.ProcessRequest;
+import com.hachic.webi.process.dto.ProcessResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/html")
+@Tag(name = "Process API")
+public class ProcessController {
+
+    private final ProcessService filteringService;
+
+    @Operation(summary = "llm 서버에 html을 전달하고 수정된 html을 받아오는 api")
+    @PostMapping("/process")
+    public ResponseEntity<ProcessResponse> filterHtml(@RequestBody ProcessRequest request) throws IOException {
+        return ResponseEntity.ok(filteringService.filterHtml(request));
+    }
+
+
+
+}
