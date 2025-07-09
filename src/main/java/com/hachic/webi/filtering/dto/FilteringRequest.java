45b0df39b5fefcb087c8d44b37fa89235c506244
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-09 21:05:16 +0900

    feat: filtering 중 중간저장

--- src/main/java/com/hachic/webi/filtering/dto/FilteringRequest.java
+++ src/main/java/com/hachic/webi/filtering/dto/FilteringRequest.java
@@ -1,4 +1,10 @@
 package com.hachic.webi.filtering.dto;
 
-public record FilteringRequest() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+public record FilteringRequest(
+        @NotNull @Schema(description = "원본 html") String originalHtml,
+        @NotNull @Schema(description = "user id") @JsonProperty("user_id") String userId) {
 }
