91c102bb2bca9f13473cbf43c55945e6c4149e26
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-09 23:40:58 +0900

    feat: html process 연결 원본 html을 ai server에 넘기고 수정된 값을 response로 보내는 기능, text 수정 필요

--- src/main/java/com/hachic/webi/filtering/dto/FilteringResponse.java
+++
@@ -1,9 +0,0 @@
-package com.hachic.webi.filtering.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.v3.oas.annotations.media.Schema;
-
-public record FilteringResponse(
-        @Schema(description = "필터링된 html string") @JsonProperty("html") String filtered_html,
-        @Schema(description = "user id") @JsonProperty("user_id") String userId) {
-}
