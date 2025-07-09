ff1f9d89395deb76e78b847c09f8a11792306778
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-09 21:05:16 +0900

    feat: filtering 중 중간저장

--- src/main/java/com/hachic/webi/filtering/application/FilteringService.java
+++ src/main/java/com/hachic/webi/filtering/application/FilteringService.java
@@ -1,4 +1,40 @@
 package com.hachic.webi.filtering.application;
 
+import com.hachic.webi.filtering.dto.FilteringRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.print.attribute.standard.Media;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
 public class FilteringService {
+
+    public void filterHtml(FilteringRequest filteringRequest) {
+        RestTemplate restTemplate = new RestTemplate();
+        String aiServerUrl = "http://api.webi.click/api/process-html";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // HTML을 JSON으로 감쌈
+        Map<String, String> body = new ConcurrentHashMap<>();
+        body.put("html", filteringRequest.originalHtml());
+        body.put("text", "주민등록등본을 발급받고 싶어");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(aiServerUrl, request, String.class);
+        String filteredHtml = response.getBody();
+    }
+
+
 }
--- src/main/java/com/hachic/webi/filtering/dto/FilteringResponse.java
+++ src/main/java/com/hachic/webi/filtering/dto/FilteringResponse.java
@@ -1,4 +1,9 @@
 package com.hachic.webi.filtering.dto;
 
-public class FilteringResponse {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record FilteringResponse(
+        @Schema(description = "필터링된 html string") @JsonProperty("html") String filtered_html,
+        @Schema(description = "user id") @JsonProperty("user_id") String userId) {
 }
