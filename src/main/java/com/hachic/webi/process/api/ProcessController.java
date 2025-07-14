f57b74bfc5998b4c2a4110ec834bc4b868b5d381
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-15 00:22:00 +0900

    style: 코딩컨벤션 적용

--- src/main/java/com/hachic/webi/process/api/ProcessController.java
+++ src/main/java/com/hachic/webi/process/api/ProcessController.java
@@ -1,18 +1,23 @@
 package com.hachic.webi.process.api;
 
-import com.hachic.webi.process.application.ProcessService;
-import com.hachic.webi.process.dto.ProcessRequest;
-import com.hachic.webi.process.dto.ProcessResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import com.hachic.webi.process.application.ProcessService;
+import com.hachic.webi.process.dto.ProcessRequest;
+import com.hachic.webi.process.dto.ProcessResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+
+
 
 @RestController
 @RequiredArgsConstructor
@@ -20,12 +25,12 @@ import java.io.IOException;
 @Tag(name = "Process API")
 public class ProcessController {
 
-    private final ProcessService filteringService;
+	private final ProcessService filteringService;
 
-    @Operation(summary = "요청받은 HTML을 LLM 서버에 전달하고 필터링 결과를 응답으로 반환하는 API")
-    @PostMapping()
-    public ResponseEntity<ProcessResponse> filterHtml(@RequestBody ProcessRequest request) throws IOException {
-        return ResponseEntity.ok(filteringService.processHtml(request));
-    }
+	@Operation(summary = "요청받은 HTML을 LLM 서버에 전달하고 필터링 결과를 응답으로 반환하는 API")
+	@PostMapping()
+	public ResponseEntity<ProcessResponse> filterHtml(@RequestBody ProcessRequest request) throws IOException {
+		return ResponseEntity.ok(filteringService.processHtml(request));
+	}
 
 }
--- src/main/java/com/hachic/webi/process/application/ProcessService.java
+++ src/main/java/com/hachic/webi/process/application/ProcessService.java
@@ -1,10 +1,9 @@
 package com.hachic.webi.process.application;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hachic.webi.process.dto.ProcessRequest;
-import com.hachic.webi.process.dto.ProcessResponse;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -12,86 +11,89 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hachic.webi.process.dto.ProcessRequest;
+import com.hachic.webi.process.dto.ProcessResponse;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ProcessService {
 
-    public ProcessResponse processHtml(ProcessRequest filteringRequest) throws IOException {
+	public ProcessResponse processHtml(ProcessRequest filteringRequest) throws IOException {
 
-        // AI 서버 URL tjfwjd
-        String aiServerUrl = "http://ai.webi.click:80/api/process-html";
+		// AI 서버 URL 설정
+		String aiServerUrl = "http://ai.webi.click:80/api/process-html";
 
-        // 요청 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+		// 요청 헤더 설정
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // 요청 본문 구성 (html, text 포함)
-        Map<String, String> body = new ConcurrentHashMap<>();
-        body.put("html", filteringRequest.originalHtml());
-        // TODO: text를 유저의 요구사항으로 변경
-        body.put("text", "주민등록등본을 발급받고 싶어");
+		// 요청 본문 구성 (html, text 포함)
+		Map<String, String> body = new ConcurrentHashMap<>();
+		body.put("html", filteringRequest.originalHtml());
+		// TODO: text를 유저의 요구사항으로 변경
+		body.put("text", "주민등록등본을 발급받고 싶어");
 
-        // 요청 객체 생성
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
+		// 요청 객체 생성
+		HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
 
-        // AI 서버에 POST 요청
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(aiServerUrl, request, String.class);
-        String response = responseEntity.getBody();
+		// AI 서버에 POST 요청
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(aiServerUrl, request, String.class);
+		String response = responseEntity.getBody();
 
-        //응답에서 modified_html, message 추출
-        ObjectMapper htmlMapper = new ObjectMapper();
-        JsonNode htmlRootNode = htmlMapper.readTree(response);
+		//응답에서 modified_html, message 추출
+		ObjectMapper htmlMapper = new ObjectMapper();
+		JsonNode htmlRootNode = htmlMapper.readTree(response);
 
-        String filteredHtml = htmlRootNode
-                .path("data")
-                .path("modified_html")
-                .asText();
+		String filteredHtml = htmlRootNode
+				.path("data")
+				.path("modified_html")
+				.asText();
 
-        String message = htmlRootNode
-                .path("data")
-                .path("message")
-                .asText();
+		String message = htmlRootNode
+				.path("data")
+				.path("message")
+				.asText();
 
-        // TODO: api 분리
-        // 소켓으로 메시지 전송
-        sendMessage(filteredHtml, filteringRequest.userId(), message);
+		// TODO: api 분리
+		// 소켓으로 메시지 전송
+		sendMessage(filteredHtml, filteringRequest.userId(), message);
 
-        return ProcessResponse.of(filteredHtml, filteringRequest.userId());
-    }
+		return ProcessResponse.of(filteredHtml, filteringRequest.userId());
+	}
 
-    private void sendMessage(String filteredHtml, String userId, String message) {
+	private void sendMessage(String filteredHtml, String userId, String message) {
 
-        // 메시지 전송 대상 URL 설정
-        String socketUrl = "http://chat.webi.click:80/users/:userId/message";
+		// 메시지 전송 대상 URL 설정
+		String socketUrl = "http://chat.webi.click:80/users/:userId/message";
 
-        // 요청 헤더 설정 (JSON 형식)
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+		// 요청 헤더 설정 (JSON 형식)
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // 요청 본문 구성 (filteredHtml, userId, message 포함)
-        Map<String, String> body = new ConcurrentHashMap<>();
-        body.put("html", filteredHtml);
-        body.put("user_id", userId);
-        body.put("message", message);
+		// 요청 본문 구성 (filteredHtml, userId, message 포함)
+		Map<String, String> body = new ConcurrentHashMap<>();
+		body.put("html", filteredHtml);
+		body.put("user_id", userId);
+		body.put("message", message);
 
-        // 요청 객체 생성
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
+		// 요청 객체 생성
+		HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
 
-        // 소켓 서버에 POST 요청
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(socketUrl, request, String.class);
+		// 소켓 서버에 POST 요청
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity(socketUrl, request, String.class);
 
-        if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            System.out.println(responseEntity.getBody());
-        } else {
-            // TODO: 에러처리
-            System.err.println(responseEntity.getBody());
-        }
-    }
+		if (responseEntity.getStatusCode().is2xxSuccessful()) {
+			System.out.println(responseEntity.getBody());
+		} else {
+			// TODO: 에러처리
+			System.err.println(responseEntity.getBody());
+		}
+	}
 }
 
