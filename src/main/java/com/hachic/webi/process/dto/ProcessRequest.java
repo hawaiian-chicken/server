9e1ae9f234be71c7a73666ac440a33fc49b9a461
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-15 00:22:00 +0900

    style: 코딩컨벤션 적용

--- src/main/java/com/hachic/webi/process/dto/ProcessRequest.java
+++ src/main/java/com/hachic/webi/process/dto/ProcessRequest.java
@@ -1,10 +1,11 @@
 package com.hachic.webi.process.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record ProcessRequest(
-        @NotNull @Schema(description = "원본 html") @JsonProperty("html") String originalHtml,
-        @NotNull @Schema(description = "user id") @JsonProperty("user_id") String userId) {
+		@NotNull @Schema(description = "원본 html") @JsonProperty("html") String originalHtml,
+		@NotNull @Schema(description = "user id") @JsonProperty("user_id") String userId) {
 }
--- src/main/java/com/hachic/webi/process/dto/ProcessResponse.java
+++ src/main/java/com/hachic/webi/process/dto/ProcessResponse.java
@@ -1,13 +1,14 @@
 package com.hachic.webi.process.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ProcessResponse(
-        @Schema(description = "필터링된 html string") @JsonProperty("html") String filtered_html,
-        @Schema(description = "user id") @JsonProperty("user_id") String userId) {
+		@Schema(description = "필터링된 html string") @JsonProperty("html") String filteredHtml,
+		@Schema(description = "user id") @JsonProperty("user_id") String userId) {
 
-    public static ProcessResponse of(String filtered_html, String userId) {
-        return new ProcessResponse(filtered_html, userId);
-    }
+	public static ProcessResponse of(String filteredHtml, String userId) {
+		return new ProcessResponse(filteredHtml, userId);
+	}
 }
