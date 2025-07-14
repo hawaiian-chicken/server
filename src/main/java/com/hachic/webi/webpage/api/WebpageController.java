74ac9af837cf1ab3b68031752e488c6e3c2fc1a1
yeeunli <lye920@g.hongik.ac.kr>
2025-07-14 23:28:33 +0900

    fix: 주석 추가

--- src/main/java/com/hachic/webi/webpage/api/WebpageController.java
+++ src/main/java/com/hachic/webi/webpage/api/WebpageController.java
@@ -3,6 +3,8 @@ package com.hachic.webi.webpage.api;
 import com.hachic.webi.application.WebpageService;
 import com.hachic.webi.dto.WebpageRequest;
 import com.hachic.webi.dto.WebpageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/save-html")
+@Tag(name = "webpage", description = "html 페이지 상호작용")
 public class WebpageController {
 
     // uuid 반환
@@ -20,6 +23,7 @@ public class WebpageController {
         this.webpageService = webpageService;
     }
 
+    @Operation(summary = "웹페이지 상호작용", description = "html 페이지 저장 후 uuid 반환")
     @PostMapping
     public ResponseEntity<WebpageResponse> saveWebpage(@RequestBody WebpageRequest webpageRequest) {
 
