edadaee9d6701ef8e5d51cfb36756234c480c1b2
yeeunli <lye920@g.hongik.ac.kr>
2025-07-15 23:01:25 +0900

    chore: 필드명 변경 api 명세서에 맞게 uuid에서 user_id로 필드명 변경

--- src/main/java/com/hachic/webi/webpage/dto/WebpageResponse.java
+++ src/main/java/com/hachic/webi/webpage/dto/WebpageResponse.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record WebpageResponse(
 
         @Schema(description = "요청 고유 ID")
-        @JsonProperty("uuid")
-        String uuid
+        @JsonProperty("user_id")
+        String userId
 ) {
 }
