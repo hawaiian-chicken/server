fff8e79c03c36d6457c1ae152886e363e83306ce
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-15 23:49:45 +0900

    style: @Valid 추가 및 필드 명 변경

--- src/main/java/com/hachic/webi/process/api/ProcessController.java
+++ src/main/java/com/hachic/webi/process/api/ProcessController.java
@@ -14,6 +14,7 @@ import com.hachic.webi.process.dto.ProcessResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 
@@ -25,12 +26,12 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "Process API")
 public class ProcessController {
 
-	private final ProcessService filteringService;
+	private final ProcessService processService;
 
 	@Operation(summary = "요청받은 HTML을 LLM 서버에 전달하고 필터링 결과를 응답으로 반환하는 API")
 	@PostMapping()
-	public ResponseEntity<ProcessResponse> filterHtml(@RequestBody ProcessRequest request) throws IOException {
-		return ResponseEntity.ok(filteringService.processHtml(request));
+	public ResponseEntity<ProcessResponse> filterHtml(@Valid @RequestBody ProcessRequest request) throws IOException {
+		return ResponseEntity.ok(processService.processHtml(request));
 	}
 
 }
