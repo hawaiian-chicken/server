df280d6d71695a809e15205420c6bc0cff9c59fb
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-16 14:24:33 +0900

    feat: ai 메시지를 디비에 저장하도록 수정

--- src/main/java/com/hachic/webi/process/dto/ProcessRequest.java
+++ src/main/java/com/hachic/webi/process/dto/ProcessRequest.java
@@ -6,6 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record ProcessRequest(
-		@NotNull @Schema(description = "원본 html") @JsonProperty("html") String originalHtml,
+		@NotNull @Schema(description = "원본 html") @JsonProperty("webpage_uuid") String webpageUuid,
 		@NotNull @Schema(description = "user id") @JsonProperty("user_id") String userId) {
 }
