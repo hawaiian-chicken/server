00d59167a265ef9b50ceb50c266607f68c5e170d
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-25 17:48:11 +0900

    refactor: 명세에 맞게 타입 수정

---
+++ src/main/java/com/hachic/webi/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.hachic.webi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
--- src/main/java/com/hachic/webi/webpage/dao/WebpageRepository.java
+++ src/main/java/com/hachic/webi/webpage/dao/WebpageRepository.java
@@ -2,6 +2,7 @@ package com.hachic.webi.webpage.dao;
 
 import java.util.Optional;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -23,11 +24,11 @@ public interface WebpageRepository extends MongoRepository<Webpage, String> {
 
 	/**
 	 * userId로 해당되는 웹페이지의 html을 가져온다
-	 * {'userId': ?0}: userId필드가 첫 번째 매개변수와 같은 문서를 찾아라
+	 * {'_id': ?0}: _id필드가 첫 번째 매개변수와 같은 문서를 찾아라
 	 * {'html': 1}: 1->포함, 0->제외, html필드만 조회해라
-	 * @param userId user_id
+	 * @param id webpage_id
 	 * @return 웹 페이지의 html Optional
 	 */
-	@Query(value = "{'userId': ?0}", fields = "{'html': 1, '_id': 0}")
-	Optional<HtmlDto> findHtmlByUserId(String userId);
+	@Query(value = "{'_id': ?0}", fields = "{'html': 1, '_id': 0}")
+	Optional<HtmlDto> findHtmlById(ObjectId id);
 }
