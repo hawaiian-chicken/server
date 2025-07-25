de45015257d2591a1594c00dd2d066f52eb090df
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-25 17:59:53 +0900

    chore: 주석 추가 및 형식 통일

--- src/main/java/com/hachic/webi/config/MongoConfig.java
+++ src/main/java/com/hachic/webi/config/MongoConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 /**
- * MongoDB Auditing을 활성화하는 Configuration 클래스입니다.
- * @CreatedDate 등의 필드를 자동으로 주입할 수 있도록 합니다.
+ * MongoDB Auditing을 활성화하는 Configuration 클래스입니다
+ * @CreatedDate 등의 필드를 자동으로 주입할 수 있도록 합니다
  */
 @Configuration
 @EnableMongoAuditing
--- src/main/java/com/hachic/webi/config/RestTemplateConfig.java
+++ src/main/java/com/hachic/webi/config/RestTemplateConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * RestTemplate을 스프링 빈으로 등록하는 설정 클래스입니다.
+ * RestTemplate을 스프링 빈으로 등록하는 설정 클래스입니다
  */
 @Configuration
 public class RestTemplateConfig {
