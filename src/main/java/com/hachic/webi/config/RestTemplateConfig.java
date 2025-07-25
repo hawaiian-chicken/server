9f4f00a43bca82de09d9d58a18be1d1454a5e463
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-07-25 17:52:39 +0900

    chore: config 파일 주석 추가

--- src/main/java/com/hachic/webi/config/RestTemplateConfig.java
+++ src/main/java/com/hachic/webi/config/RestTemplateConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * RestTemplate을 스프링 빈으로 등록하는 설정 클래스입니다.
+ */
 @Configuration
 public class RestTemplateConfig {
 
