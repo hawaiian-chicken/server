768cb4b196e636efa934c7024a5334d63777ef44
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-08-06 00:55:37 +0900

    fix: 수정된 ai 응답에 맞게 수정 1. ProcessResult 도메인 - aiMessage 필드와 통일성을 위해 userRequest필드를 userMessage로 이름 변경 - modifiedHtml 삭제 - List<Actions> 필드 추가, action, tag, message를 담은 Actions 리스트
    2. ProcessRequest DTO 수정
    - 요청메시지에서 유저 요청 메시지를 받도록 수정 (userMessage 필드 추가)
    
    3. ProcessResponse DTO 수정
    - modifiedHtml 필드가 사라짐에 따라 수정된 html이 아닌 Actions 리스트를 응답으로 반환하도록 수정
    
    4. ProcessService - socket 전송
    - 기존의 수정된 html을 전송하는 로직에서 action, tag, message를 담은 Actions 리스트를 전송하도록 수정
    
    5. ProcessService
    - 수정된 ai 서버의 응답에 맞게 값을 가져올 때의 path 수정
    - modifiedHtml을 삭제하고 List<Actions>를 추가해 값을 각각 저장하도록 함
    
    6. application.properties
    - ai 서버 url 변경 (process-html -> process-html-with-actions)

--- src/main/java/com/hachic/webi/process/domain/ProcessResult.java
+++ src/main/java/com/hachic/webi/process/domain/ProcessResult.java
@@ -1,12 +1,15 @@
 package com.hachic.webi.process.domain;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+import com.hachic.webi.process.dto.Actions;
 
 import lombok.Getter;
 
@@ -20,8 +23,8 @@ public class ProcessResult {
 	@Field("user_id")
 	private final String userId;
 
-	@Field("user_request")
-	private final String userRequest;
+	@Field("user_message")
+	private final String userMessage;
 
 	@Field("webpage_id")
 	private final ObjectId webpageId;
@@ -29,10 +32,13 @@ public class ProcessResult {
 	@Field("request_type")
 	private final String requestType;
 
-	@Field("modified_html")
-	private final String modifiedHtml;
+/*	@Field("modified_html")
+	private final String modifiedHtml;*/
 
-	private final String message;
+	private final List<Actions> actions;
+
+	@Field("ai_message")
+	private final String aiMessage;
 
 	@Field("created_at")
 	@CreatedDate
@@ -40,16 +46,18 @@ public class ProcessResult {
 
 	public ProcessResult(
 			String userId,
-			String userRequest,
+			String userMessage,
 			ObjectId webpageId,
 			String requestType,
-			String modifiedHtml,
-			String message) {
+			//String modifiedHtml,
+			List<Actions> actions,
+			String aiMessage) {
 		this.userId = userId;
-		this.userRequest = userRequest;
+		this.userMessage = userMessage;
 		this.webpageId = webpageId;
 		this.requestType = requestType;
-		this.modifiedHtml = modifiedHtml;
-		this.message = message;
+		//this.modifiedHtml = modifiedHtml;
+		this.actions = actions;
+		this.aiMessage = aiMessage;
 	}
 }
--- src/main/java/com/hachic/webi/process/dto/ProcessRequest.java
+++ src/main/java/com/hachic/webi/process/dto/ProcessRequest.java
@@ -7,5 +7,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record ProcessRequest(
 		@NotNull @Schema(description = "원본 html") @JsonProperty("webpage_id") String webpageId,
+		@NotNull @Schema(description = "유저가 보내는 메시지") @JsonProperty("message") String userMessage,
 		@NotNull @Schema(description = "user id") @JsonProperty("user_id") String userId) {
 }
