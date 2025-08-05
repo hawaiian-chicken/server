afed1c93d48a3c47da39e9a9133b2eb881a8297c
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-08-06 00:55:37 +0900

    fix: 수정된 ai 응답에 맞게 수정 1. ProcessResult 도메인 - aiMessage 필드와 통일성을 위해 userRequest필드를 userMessage로 이름 변경 - modifiedHtml 삭제 - List<Actions> 필드 추가, action, tag, message를 담은 Actions 리스트
    2. ProcessRequest DTO 수정
    - 요청메시지에서 유저 요청 메시지를 받도록 수정 (userMessage 필드 추가)
    
    3. ProcessResponse DTO 수정
    - modifiedHtml 필드가 사라짐에 따라 수정된 html이 아닌 Actions 리스트를 응답으로 반환하도록 수정
    
    4. ProcessService - socket 전송
    - 기존의 수정된 html을 전송하는 로직에서 action, tag, message를 담은 Actions 리스트를 전송하도록 수정
    
    5. ProcessService
    - 수정된 ai 서버의 응답에 맞게 값을 가져올 때의 path 수정
    - modifiedHtml을 삭제하고 List<Actions>를 추가해 값을 각각 저장하도록 함
    
    6. application.properties
    - ai 서버 url 변경 (process-html -> process-html-with-actions)

--- src/main/java/com/hachic/webi/process/dto/ProcessResponse.java
+++ src/main/java/com/hachic/webi/process/dto/ProcessResponse.java
@@ -1,14 +1,16 @@
 package com.hachic.webi.process.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ProcessResponse(
-		@Schema(description = "필터링된 html string") @JsonProperty("html") String filteredHtml,
+		@Schema(description = "필터링된 actions 리스트(action, tag, message)")@JsonProperty("actions") List<Actions> actions,
 		@Schema(description = "user id") @JsonProperty("user_id") String userId) {
 
-	public static ProcessResponse of(String filteredHtml, String userId) {
-		return new ProcessResponse(filteredHtml, userId);
+	public static ProcessResponse of(List<Actions> actions, String userId) {
+		return new ProcessResponse(actions, userId);
 	}
 }
