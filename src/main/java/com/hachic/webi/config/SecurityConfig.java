4dfcea87baa7e74d71282306acd7eef0a9c77ea1
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-08-27 02:31:35 +0900

    chore: 로컬 관련 설정을 배포 버전으로 변경

--- src/main/java/com/hachic/webi/config/SecurityConfig.java
+++ src/main/java/com/hachic/webi/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:8080"));
+		config.setAllowedOrigins(List.of("https://api.webi.click"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
