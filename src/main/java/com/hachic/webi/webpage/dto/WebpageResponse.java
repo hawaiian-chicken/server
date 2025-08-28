6147a39eb9c2163716694fc68a2de20c5c071c97
yeeunli <lye920@g.hongik.ac.kr>
2025-08-28 23:19:00 +0900

    refactor: WebpageResponse에 from 정적 팩토리 메서드 추가

--- src/main/java/com/hachic/webi/webpage/dto/WebpageResponse.java
+++ src/main/java/com/hachic/webi/webpage/dto/WebpageResponse.java
@@ -1,6 +1,7 @@
 package com.hachic.webi.webpage.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hachic.webi.webpage.domain.Webpage;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -18,4 +19,17 @@ public record WebpageResponse(
 		String webpageId
 
 ) {
+
+	/**
+	 * Webpage 엔티티를 WebpageResponse DTO로 변환한다.
+	 *
+	 * @param webpage Webpage 엔티티
+	 * @return 변환된 WebpageResponse DTO
+	 */
+	public static WebpageResponse from(Webpage webpage) {
+		return new WebpageResponse(
+				webpage.getUserId(),
+				webpage.getId().toHexString()
+		);
+	}
 }
