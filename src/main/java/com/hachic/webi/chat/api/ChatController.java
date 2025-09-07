6a94ac6f9faad37a6512480c5fc38ea40c5eab01
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-09-08 01:02:54 +0900

    feat: 채팅 개별 조회 api 구현

--- src/main/java/com/hachic/webi/chat/api/ChatController.java
+++ src/main/java/com/hachic/webi/chat/api/ChatController.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hachic.webi.chat.application.ChatService;
 import com.hachic.webi.chat.dto.ChatRoom;
-import com.hachic.webi.chat.dto.ConversationResponse;
+import com.hachic.webi.chat.dto.ConversationDetail;
+import com.hachic.webi.chat.dto.MessageDetail;
 
 import lombok.RequiredArgsConstructor;
 
@@ -31,11 +33,21 @@ public class ChatController {
 	}
 
 	@GetMapping()
-	public ResponseEntity<List<ConversationResponse>> getConversations(
+	public ResponseEntity<List<ConversationDetail>> getConversations(
 			// @AuthenticationPrincipal OAuth2ResourceServerProperties.Jwt jwt
 	) {
 		// String userId = jwt.getSubject();
 		String userId = "hachic";
 		return ResponseEntity.ok(chatService.getConversations(userId));
+	}
+
+	@GetMapping("/{conversation_id}")
+	public ResponseEntity<List<MessageDetail>> getMessages(
+			@PathVariable("conversation_id") Long conversationId
+			// @AuthenticationPrincipal OAuth2ResourceServerProperties.Jwt jwt
+	) {
+		// String userId = jwt.getSubject();
+		String userId = "hachic";
+		return ResponseEntity.ok(chatService.getMessages(conversationId));
 	}
 }
--- src/main/java/com/hachic/webi/chat/dto/ConversationDetail.java
+++ src/main/java/com/hachic/webi/chat/dto/ConversationDetail.java
@@ -6,22 +6,22 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hachic.webi.chat.domain.Conversation;
 
-public record ConversationResponse(
+public record ConversationDetail(
 		Long id,
 		String title,
 		List<String> tags,
 		@JsonProperty("last_message_at") Instant lastMessageAt,
 		@JsonProperty("message_count") int msgCount
 ) {
-	public ConversationResponse(Long id, String title, List<String> tags, Instant lastMessageAt, int msgCount) {
+	public ConversationDetail(Long id, String title, List<String> tags, Instant lastMessageAt, int msgCount) {
 		this.id = id;
 		this.title = title;
 		this.tags = tags;
 		this.lastMessageAt = lastMessageAt;
 		this.msgCount = msgCount;
 	}
-	public static ConversationResponse from(Conversation conversation) {
-		return new ConversationResponse(
+	public static ConversationDetail from(Conversation conversation) {
+		return new ConversationDetail(
 				conversation.getId(),
 				conversation.getTitle(),
 				conversation.getTags(),
