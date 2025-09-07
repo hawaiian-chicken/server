e50ed14fadb2e285b03696419c98e4380c955d8a
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-09-08 01:44:09 +0900

    refactor: 주석 및 메서드 설명 추가

--- src/main/java/com/hachic/webi/chat/api/ChatController.java
+++ src/main/java/com/hachic/webi/chat/api/ChatController.java
@@ -14,40 +14,47 @@ import com.hachic.webi.chat.dto.ChatRoom;
 import com.hachic.webi.chat.dto.ConversationDetail;
 import com.hachic.webi.chat.dto.MessageDetail;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "Chat API", description = "채팅방 관련 API")
 @RequestMapping("/chat")
 public class ChatController {
 
 	private final ChatService chatService;
 
 	@PostMapping("/chat-room")
+	@Operation(summary = "채팅방을 생성하고 채팅방 ID를 반환하는 API")
 	public ResponseEntity<ChatRoom> createChatRoom(
 			// @AuthenticationPrincipal OAuth2ResourceServerProperties.Jwt jwt
 	) {
+		// TODO: 로그인 구현 후 userId 변경
 		// String userId = jwt.getSubject();
 		String userId = "hachic";
 		return ResponseEntity.ok(chatService.createConversation(userId));
 	}
 
 	@GetMapping()
+	@Operation(summary = "현재 로그인된 유저의 채팅방 목록을 보여주는 API")
 	public ResponseEntity<List<ConversationDetail>> getConversations(
 			// @AuthenticationPrincipal OAuth2ResourceServerProperties.Jwt jwt
 	) {
+		// TODO: 로그인 구현 후 userId 변경
 		// String userId = jwt.getSubject();
 		String userId = "hachic";
 		return ResponseEntity.ok(chatService.getConversations(userId));
 	}
 
 	@GetMapping("/{conversation_id}")
+	@Operation(summary = "특정 채팅방의 상세 대화 내역을 보여주는 API")
 	public ResponseEntity<List<MessageDetail>> getMessages(
 			@PathVariable("conversation_id") Long conversationId
 			// @AuthenticationPrincipal OAuth2ResourceServerProperties.Jwt jwt
 	) {
-		// String userId = jwt.getSubject();
-		String userId = "hachic";
+		// TODO: 로그인 구현 후 userId가 conversation에 저장된 id와 같을 때만 접근 허용
 		return ResponseEntity.ok(chatService.getMessages(conversationId));
 	}
 }
--- src/main/java/com/hachic/webi/chat/dto/MessageDetail.java
+++ src/main/java/com/hachic/webi/chat/dto/MessageDetail.java
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hachic.webi.chat.domain.Message;
 import com.hachic.webi.chat.domain.Role;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record MessageDetail(
-		@JsonProperty("message_id") String messageId,
-		Role role,
-		String content,
-		@JsonProperty("created_at") Instant createdAt
+		@Schema(description = "메시지 ID") @JsonProperty("message_id") String messageId,
+		@Schema(description = "USER or ASSISTANT") Role role,
+		@Schema(description = "메시지 내용") String content,
+		@Schema(description = "메시지 전송 시각") @JsonProperty("created_at") Instant createdAt
 ) {
 	public MessageDetail(String messageId, Role role, String content, Instant createdAt) {
 		this.messageId = messageId;
