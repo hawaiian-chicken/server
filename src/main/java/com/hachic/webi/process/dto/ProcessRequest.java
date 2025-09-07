583f95373df01af2f8dc75de42769e67aae64887
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-09-07 19:01:51 +0900

    refactor: ProcessResult 컬렉션 대신 Message와 Conversation 컬렉션에 저장하도록 변경

--- src/main/java/com/hachic/webi/process/dto/ProcessRequest.java
+++ src/main/java/com/hachic/webi/process/dto/ProcessRequest.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record ProcessRequest(
+		@NotNull @Schema(description = "채팅방 번호") @JsonProperty("conversation_id") Long conversationId,
 		@NotNull @Schema(description = "원본 html") @JsonProperty("webpage_id") String webpageId,
-		@NotNull @Schema(description = "유저가 보내는 메시지") @JsonProperty("message") String userMessage,
-		@NotNull @Schema(description = "user id") @JsonProperty("user_id") String userId) {
+		@NotNull @Schema(description = "유저가 보내는 메시지") @JsonProperty("message") String userMessage) {
 }
