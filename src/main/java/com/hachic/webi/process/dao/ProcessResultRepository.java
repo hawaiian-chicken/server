84494d5b8ccea843b87fa30289514e99454315c5
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-09-07 19:05:05 +0900

    chore: ProcessResult 관련 파일 제거

--- src/main/java/com/hachic/webi/process/dao/ProcessResultRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hachic.webi.process.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.hachic.webi.process.domain.ProcessResult;
-
-public interface ProcessResultRepository extends MongoRepository<ProcessResult, String> {
-}
