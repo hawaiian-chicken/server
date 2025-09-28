5ede5dfef081b0e4c34ad032efb7c7fa48068924
ybkang1108 <ybkang1108@g.hongik.ac.kr>
2025-09-28 16:56:35 +0900

    refactor: login 응답을 json이 아닌 html로 변경

--- src/main/java/com/hachic/webi/oauth/api/OauthController.java
+++ src/main/java/com/hachic/webi/oauth/api/OauthController.java
@@ -1,5 +1,7 @@
 package com.hachic.webi.oauth.api;
 
+import java.io.IOException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hachic.webi.oauth.application.OauthService;
-import com.hachic.webi.oauth.dto.UserResponse;
+import com.hachic.webi.oauth.dto.UserInfo;
 import com.hachic.webi.oauth.helper.constants.SocialLoginType;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -42,24 +44,37 @@ public class OauthController {
 	}
 
 	@Operation(
-			summary = "소셜 로그인 콜백 처리 (백엔드에서 사용 X)",
+			summary = "소셜 로그인 콜백 처리",
 			description = "사용자가 소셜 로그인 후 콜백 URL로 받은 코드를 통해 액세스 토큰을 요청",
 			operationId = "handleSocialLoginCallback"
 	)
 	@GetMapping("/{socialLoginType}/callback")
-	public ResponseEntity<?> callback(
+	public void callback(
 			@PathVariable(name = "socialLoginType") SocialLoginType socialLoginType,
-			@RequestParam(name = "code") String code) throws JsonProcessingException {
+			@RequestParam(name = "code") String code,
+			HttpServletResponse response) throws IOException {
 		log.info(">> 소셜 로그인 API 서버로부터 받은 code :: {}", code);
 
 		// 액세스 토큰을 받아온 후 사용자 정보를 DB에 저장
-		UserResponse user = oauthService.requestAccessTokenAndSaveUser(socialLoginType, code);
-		if (user != null) {
-			log.info(">> 사용자 정보 DB 저장 완료:: {}", user.name());
-			return ResponseEntity.ok(user);
-		} else {
-			log.error(">> 사용자 정보 저장 실패");
-			return ResponseEntity.status(500).body("사용자 정보 저장 실패");
-		}
+		UserInfo user = oauthService.requestAccessTokenAndSaveUser(socialLoginType, code);
+
+		String script = String.format("""
+				<script>
+					localStorage.setItem("accessToken", "%s");
+					localStorage.setItem("name", "%s");
+					localStorage.setItem("provider", "%s");
+					window.location.href = "/login-success";
+				</script>
+				""", user.accessToken(), user.name(), user.provider());
+		response.setContentType("text/html; charset=UTF-8");
+		response.getWriter().write(script);
+
+//		if (user != null) {
+//			log.info(">> 사용자 정보 DB 저장 완료:: {}", user.name());
+//			//return ResponseEntity.ok(user);
+//		} else {
+//			log.error(">> 사용자 정보 저장 실패");
+//			//return ResponseEntity.status(500).body("사용자 정보 저장 실패");
+//		}
 	}
 }
